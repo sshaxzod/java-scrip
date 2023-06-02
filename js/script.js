a4d4e22f716c354fc4c1128ea94accb34d446dac
sshaxzod <127496475+sshaxzod@users.noreply.github.com>
2023-06-02 13:51:49 +0500

    .

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let myName = "Mining ismim "
+let myFamily ="mining famiyam"
+let myage ="mining yshim esa"
+let myeduction = "o'qish joyim"
+let myhobby = "Qiziqqan narsam"
+
+
+let Name = "Shaxzod"
+let Family ="Raxmonov"
+let age ="21 da"
+let eduction = "University"
+let hobby = "Counter-strike game"
+console.log(myName + " " + Name +  " " + myFamily + " " + Family + " "  + myage + " " + age + " " + myeduction + " " + eduction + " " + hobby   )
